67c51e0f6daf5623135d42a7e237a2b4f252e56c
Suhartanto74 <Suhartanto@gmail.com>
2025-07-30 10:55:02 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753847701042

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_2cfa0647.js",
+  "static/chunks/node_modules_0c66244f._.js",
+  "static/chunks/src_405bde5b._.js"
+],
+    source: "dynamic"
+});
